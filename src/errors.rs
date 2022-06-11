9d7080ba2160b456e2a7eb01e86ac4edbb675a3f
Raheel Ahmad <ahmadraheel@gmail.com>
2022-06-11 00:55:10 -0700

    error tweak

--- src/errors.rs
+++ src/errors.rs
@@ -4,6 +4,8 @@ pub enum Error {
     DBAccess(String),
 }
 
+impl std::error::Error for Error {}
+
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
